45bd447e63413eac3a75ed602f98978ed1b48ea2
HPRP01 <55462701+HPRP01@users.noreply.github.com>
2022-10-17 22:25:28 -0400

    Update bopItDesign.ino

--- src/bopItDesign/bopItDesign.ino
+++ src/bopItDesign/bopItDesign.ino
@@ -38,6 +38,9 @@ void setup() {
     // Connect SCL to pin 26 (1st on right)
     // Connect SDA to pin 27 (2nd on right)
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.display();
+    delay(1000);
+    display.clearDisplay();
 
   // Start Button
     // Connect Start Button to D9 (pin 15 bottom right)
@@ -54,7 +57,7 @@ void setup() {
   // Rotation Sensor
     // Connect SCL to pin 26 (1st on right)
     // Connect SDA to pin 27 (2nd on right)
-    mpu.begin(MPU6050_SCLAE_2000DPS, MPU6050_RANGE_2G);
+    mpu.begin(); 
 
   // Speaker
     // Connect speaker to pin D10 (pin 16 2nd from bottom right)
@@ -65,6 +68,78 @@ void setup() {
 }
 
 void loop() {
+  int  turnTime    = 1000;
+  bool playingGame = false;
   
+  bool gameStarted = digitalRead(START_BUTTON);
 
+  if(gameStarted == 1)
+  {
+    display.clearDisplay();
+    bool playingGame = true;
+    int  score       = 0;
+  
+    while(playingGame == true)
+    {
+      bool actionCompleted = false;
+      int  action          = random(1,4);
+
+      if(action == 1) {
+        // Play Tone 1
+        display.println("Push It");
+        display.println(score);
+        display.display();
+      } else if(action == 2) {
+        // Play Tone 2
+        display.println("Cover It");
+        display.println(score);
+        display.display();
+      } else {
+        // Play Tone 3
+        display.println("Spin It");
+        display.println(score);
+        display.display();
+      }
+
+      int startTime   = millis();
+      int elapsedTime = millis() - startTime;
+      while(elapsedTime < turnTime)
+      {
+        elapsedTime = millis() - startTime;
+        
+        if(action == 1)
+        {
+          // PUSH IT
+          bool pushedIt = digitalRead(ACTION_BUTTON);
+
+          if(pushedIt == 1)
+          {
+            actionCompleted = true;
+            delay(200);
+            break;
+          }
+        }
+        else if(action == 2)
+        {
+          // COVER IT
+        }
+        else if(action == 3)
+        {
+          // SPIN IT
+        }
+      }
+      
+      if(actionCompleted == false)
+      {
+        // GAME OVER
+        display.println("GAME OVER");
+        display.println("Final Score: ");
+        display.println(score);
+        display.display();
+      }
+
+      score    = score + 1;
+      turnTime = turnTime - 50;
+    } //End While 
+  } //End If gameStarted
 }
