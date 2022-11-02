3f404bea773376316c6464bac8d349d6e3eebf54
HPRP01 <55462701+HPRP01@users.noreply.github.com>
2022-11-02 11:06:26 -0400

    update comments

--- src/bopItDesign/bopItDesign.ino
+++ src/bopItDesign/bopItDesign.ino
@@ -16,10 +16,10 @@
 #define PHOTORESISTOR A0
 #define SPEAKER 10
 
-#define LEDPIN 7
-#define LEDPIN_FAIL 6
+#define LEDPIN 7      // SUCCESS LED
+#define LEDPIN_FAIL 6 // FAILURE LED
 
-#define COVER_IT_CUTOFF 200
+#define COVER_IT_CUTOFF 200 // Configure cutoff for Photoresistor
 
 // Define I2C Addresses
 #define DISPLAY_I2C 0x3C
@@ -33,6 +33,7 @@ Adafruit_MPU6050 mpu;
 void setup() {
   Serial.begin(9600);
 
+  // Initialize MPU (accelerometer/gyroscope)
   if (!mpu.begin()) {
     digitalWrite(LEDPIN, HIGH);
     while (1) {
@@ -89,45 +90,60 @@ void loop() {
     display.setTextColor(WHITE);
     display.setTextSize(2);
     display.setCursor(0,0);
+
+    // Set output LEDs LOW
     digitalWrite(LEDPIN_FAIL, LOW);
     digitalWrite(LEDPIN, LOW);
+
+    // Wait for game to start
     delay(1000);
   
     while(playingGame == true)
     {
-      bool actionCompleted = false;
-      int  action          = random(1,4);
       sensors_event_t a, g, temp;
+
+      // Initialize action completed to false
+      bool actionCompleted = false;
+
+      // Pick random action
+      int  action          = random(1,4);
+
+      // Clear display
       display.clearDisplay();
       display.setCursor(0,0);
       
 
       if(action == 1) {
+        // Display PUSH IT command
         display.println(F("Push It"));
         display.println(score);
         display.display();
-        
+
+        // Give tone MID -> HIGH
         tone(10, 523, 500);
         delay(500);
         tone(10, 1047, 500);
         delay(500);
         noTone(10);
       } else if(action == 2) {
-        // Play Tone 2
+        // Display COVER IT command
         display.println(F("Cover It"));
         display.println(score);
         display.display();
-        
+
+        // Give tone MID -> MID
         tone(10, 523, 500);
         delay(500);
         tone(10, 523, 500);
         delay(500);
         noTone(10);
       } else {
+        // Display SPIN IT command
         display.println(F("Spin It"));
         display.println(score);
         display.display();
-        
+
+        // Give tone MID -> LOW
         tone(10, 523, 500);
         delay(500);
         tone(10, 262, 500);
@@ -135,23 +151,29 @@ void loop() {
         noTone(10);
       }
 
+      // Set start time to current time
       int startTime   = millis();
       int elapsedTime = millis() - startTime;
+
+      // While time still left in turn
       while(elapsedTime < turnTime)
       {
+        // Set elapsed time to current time - start time
         elapsedTime = millis() - startTime;
-        
+
+        // PUSH IT
         if(action == 1)
         {
-          // PUSH IT
           bool pushedIt = digitalRead(ACTION_BUTTON);
 
+          // If cover it -> go to game over
           if(analogRead(PHOTORESISTOR) < COVER_IT_CUTOFF)
           {
             actionCompleted = false;
             break;
           }
 
+          // If spin it -> go to game over
           mpu.getEvent(&a, &g, &temp);
           if(a.acceleration.x > 2)
           {
@@ -160,15 +182,18 @@ void loop() {
             
           }
 
+          // If push it -> go to success
           if(pushedIt == 1)
           {
             actionCompleted = true;
             break;
           }
         }
+
+        // COVER IT
         else if(action == 2)
         {
-          // COVER IT
+          // If push it -> go to game over
           bool pushedIt = digitalRead(ACTION_BUTTON);
           if(pushedIt == 1)
           {
@@ -176,6 +201,7 @@ void loop() {
             break;
           }
 
+          // If spin it -> go to game over
           mpu.getEvent(&a, &g, &temp);
           if(a.acceleration.x > 2)
           {
@@ -183,28 +209,34 @@ void loop() {
             break;
             
           }
-          
+
+          // If cover it -> go to success
           if(analogRead(PHOTORESISTOR) < COVER_IT_CUTOFF)
           {
             actionCompleted = true;
             break;
           }
         }
+
+        // SPIN IT
         else if(action == 3)
         {
+          // If Cover it -> go to game over
           if(analogRead(PHOTORESISTOR) < COVER_IT_CUTOFF)
           {
             actionCompleted = false;
             break;
           }
 
+          // If push it -> go to game over
           bool pushedIt = digitalRead(ACTION_BUTTON);
           if(pushedIt == 1)
           {
             actionCompleted = false;
             break;
           }
-          
+
+          // If spin it -> go to success
           mpu.getEvent(&a, &g, &temp);
           if(a.acceleration.x > 2)
           {
@@ -212,47 +244,63 @@ void loop() {
             break;
             
           }
-          // SPIN IT
-          
         }
       }
-      
+
+      // GAME OVER
+      // If action was not completed or incorrect action
       if(actionCompleted == false)
       {
-        // GAME OVER
+        // Display final score and Game Over message
         display.clearDisplay();
         display.setCursor(0,0);
         display.println(F("GAME OVER"));
         display.println("Score: ");
         display.println(score);
         display.display();
+
+        // Set failure LED HIGH
         digitalWrite(LEDPIN_FAIL, HIGH);
         break;
       }
 
-      score    = score + 1;
+      // Increase Score
+      score = score + 1;
+
+      // Clear instruction from display and show new score
       display.clearDisplay();
       display.setCursor(0,0);
       display.println("");
       display.println(score);
       display.display();
+      
+      // Set success LED HIGH
       digitalWrite(LEDPIN, HIGH);
+
+      // Wait for the rest of turn time then set success LED LOW
       delay(turnTime - elapsedTime);
       digitalWrite(LEDPIN, LOW);
 
+      // Decrease the turn time be 10ms at end of round
+      turnTime = turnTime - 10;
+
+      // Check for if score limit has been reached (set to 99)
       if(score == 99)
       {
+        // Set both output LEDs HIGH
         digitalWrite(LEDPIN, HIGH);
         digitalWrite(LEDPIN_FAIL, HIGH);
+
+        // Write score and you win to display
         display.clearDisplay();
         display.setCursor(0,0);
         display.println("You Win!");
         display.println(score);
         display.display();
+
+        // Break out of game loop
         break;
       }
-      
-      turnTime = turnTime - 10;
     } //End While 
   } //End If gameStarted
 }
